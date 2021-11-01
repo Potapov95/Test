277533d97dfba644b7c5d2f4a55e2546b6134e05
Potapov95 <93530770+Potapov95@users.noreply.github.com>
2021-11-01 17:15:25 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,5 @@
-# Test
-Print(5)
+money	=	int(input('сколько денег ,в рублях, Вы тратите в месяц: '))
+n_miles	=	2  # две бесплатные авиамили
+miles_per_month	=	money	+	n_miles
+distance	=	215
+print(distance*2/miles_per_month)
